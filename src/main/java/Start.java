04b16050385bb5e4c0c1ec677103631bda920b88
BlackDev <BlackDevReal@proton.me>
2026-01-05 09:58:19 +0100

    feat: OSX & Linux natives and dev launch support (#3)

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -2,18 +2,70 @@ import net.minecraft.client.main.Main;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Start {
+    public static void main(String[] args) {
+        String currentDir = System.getProperty("user.dir");
+        System.out.println("Current working directory: " + currentDir);
 
-    public static void main(String[] args)
-    {
-        System.setProperty("org.lwjgl.librarypath", new File("../natives/" + (System.getProperty("os.name").startsWith("Windows") ? "windows" : "linux")).getAbsolutePath());
+        String osName = System.getProperty("os.name").toLowerCase();
+        String osArch = System.getenv("PROCESSOR_ARCHITECTURE");
+        if (osArch == null) {
+            osArch = System.getenv("PROCESSOR_ARCHITEW6432");
+        }
+        if (osArch == null) {
+            osArch = System.getProperty("os.arch").toLowerCase();
+        }
+        String nativePath;
 
-        Main.main(concat(new String[]{"--version", "GradleMCP", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}"}, args));
+        if (osName.contains("win")) {
+            nativePath = "natives/windows";
+        } else if (osName.contains("mac")) {
+            if (osArch.contains("aarch64") || osArch.contains("arm64")) {
+                nativePath = "natives/osx_silicon";
+            } else {
+                nativePath = "natives/osx";
+            }
+        } else if (osName.contains("nix") || osName.contains("nux")) {
+            nativePath = "natives/linux";
+        } else {
+            throw new UnsupportedOperationException("Unsupported operating system: " + osName);
+        }
+
+        String absoluteNativePath = new File(new File(currentDir).getParentFile(), nativePath)
+            .getAbsolutePath();
+        File nativeDir = new File(absoluteNativePath);
+
+        if (!nativeDir.exists()) {
+            System.out.println("Default native library path not found: " + absoluteNativePath);
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter a custom path:");
+            absoluteNativePath = scanner.nextLine();
+            nativeDir = new File(absoluteNativePath);
+
+            if (!nativeDir.exists()) {
+                System.err.println("The provided custom path does not exist. Exiting.");
+                return;
+            }
+        }
+
+        System.out.println("Native library path: " + absoluteNativePath);
+
+
+        System.setProperty("org.lwjgl.librarypath", absoluteNativePath);
+
+        Main.main(concat(new String[]{
+                        "--version", "MavenMCP",
+                        "--accessToken", "0",
+                        "--assetsDir", "assets",
+                        "--assetIndex", "1.8",
+                        "--userProperties", "{}"},
+                args)
+        );
     }
 
-    public static <T> T[] concat(T[] first, T[] second)
-    {
+    public static <T> T[] concat(T[] first, T[] second) {
         T[] result = Arrays.copyOf(first, first.length + second.length);
         System.arraycopy(second, 0, result, first.length, second.length);
         return result;
